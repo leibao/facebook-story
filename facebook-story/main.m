39e3e8ad1406ce22b11f2380406ca11ee77961a5
Lei Bao <leibao@yahoo-inc.com>
2014-06-08 22:34:48 -0700

    Initial Commit

---
+++ facebook-story/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  facebook-story
+//
+//  Created by Lei Bao on 6/8/14.
+//  Copyright (c) 2014 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ facebook-storyTests/facebook_storyTests.m
@@ -0,0 +1,34 @@
+//
+//  facebook_storyTests.m
+//  facebook-storyTests
+//
+//  Created by Lei Bao on 6/8/14.
+//  Copyright (c) 2014 Yahoo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface facebook_storyTests : XCTestCase
+
+@end
+
+@implementation facebook_storyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
